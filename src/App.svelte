554dcf3f6db62b725274cacb26b244e6d4f9440b
meraseudev <meraseudev@gmail.com>
2019-12-03 16:54:39 +0900

    Section 2. Base Syntax & Core Features

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,44 @@
 <script>
-	export let name;
+	import ContactCard from './ContactCard.svelte';
+
+	let name = 'Meraseudev';
+	let title = '';
+	let description = '';
+	let image = '';
+	let age = 30;
+
+	$: uppercaseName = name.toUpperCase();
+
+	// $: console.log(name);
+
+	$: if (name === 'Meraseu') {
+		console.log('It runs');
+		age = 31;
+	}
+
+	function changeName() {
+		name = 'Meraseu';
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {uppercaseName}! my age is {age}</h1>
+	<button type="button" on:click="{ () => age += 1 }">Change Age</button>
+	<!-- <button type="button" on:click="{ changeName }">Change Name</button> -->
+	<!-- <input type="text" value="{ name }" on:input="{ (e) => { 
+		const enterValue = e.target.value;
+		name = enterValue;
+	 } }"> -->
+	 <input type="text" bind:value="{ name }">
+	 <input type="text" bind:value="{ title }">
+	 <input type="text" bind:value="{ image }">
+	 <textarea name="" id="" cols="30" rows="10" bind:value="{ description }"></textarea>
+
+	 <ContactCard 
+	 	userName="{ name }"
+		jobTitle="{ title }"
+		{ description }
+		userImage="{ image }" />
 </main>
 
 <style>
@@ -17,7 +51,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
