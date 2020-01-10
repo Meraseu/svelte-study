122b03693904a70a835eb2a43b1fb07501219061
meraseudev <meraseudev@gmail.com>
2020-01-10 15:21:35 +0900

    Section 6 ing...

--- src/App.svelte
+++ src/App.svelte
@@ -1,112 +1,41 @@
 <script>
-	import Header from './UI/Header.svelte';
-	import MeetupGrid from './Meetups/MeetupGrid.svelte';
-	import TextInput from './UI/TextInput.svelte';
-	import Button from './UI/Button.svelte';
+	import Product from './Product.svelte';
+	import Modal from './Modal.svelte';
 
-	let title = '';
-	let subtitle = '';
-	let address = '';
-	let email = '';
-	let description = '';
-	let imageUrl = '';
-
-	let meetups = [
+	let products = [
 		{
-			id: 'm1',
-			title: 'Coding Bootcamp',
-			subtitle: 'Learn to code in 2 hours',
-			description: 'In this meetup',
-			imageUrl: '',
-			address: 'Seoul',
-			contactEmail: 'gmail.com'
-		},
-		{
-			id: 'm2',
-			title: 'Coding Bootcamp',
-			subtitle: 'Learn to code in 2 hours',
-			description: 'In this meetup',
-			imageUrl: '',
-			address: 'Seoul',
-			contactEmail: 'gmail.com'
+			id: 'p1',
+			title: 'A book',
+			price: 9.99,
+			bestseller: true
 		}
-	]
-	function addMeetup() {
-		const newMeetup = {
-			id: Math.random().toString(),
-			title: title,
-			subtitle: subtitle,
-			address: address,
-			description: description,
-			imageUrl: imageUrl,
-			contactEmail: email
-		};
-		meetups = [newMeetup, ...meetups];
+	];
+
+	let showModal = false;
+	let closeable = false;
+
+	function addToCart(event) {
+		console.log(event);
+	}
+	function deleteProduct(event) {
+		console.log(event.detail);
 	}
 </script>
 
-<style>
-	main {
-		margin-top:5rem;
-	}
-	form {
-		width:30rem;
-		max-width:90%;
-		margin:auto;
-	}
-</style>
+{#each products as product}
+<Product 
+	{ ...product }
+	on:add-to-cart="{ addToCart }"
+	on:delete="{ deleteProduct }"
+/>
+{/each}
 
-<Header />
+<button type="button" on:click="{ () => showModal = true }">Show Modal</button>
 
-<main>
-	<form on:submit|preventDefault = "{ addMeetup }">
-		<TextInput 
-			id="title"
-			label="Title"
-			value={title}
-			on:input="{event => title = event.target.value}"
-			type="text"
-		/>
-		<TextInput 
-			id="subtitle"
-			label="Subtitle"
-			value={subtitle}
-			on:input="{event => subtitle = event.target.value}"
-			type="text"
-		/>
-		<TextInput 
-			id="address"
-			label="Address"
-			value={address}
-			on:input="{event => address = event.target.value}"
-			type="text"
-		/>
-		<TextInput 
-			id="imageUrl"
-			label="ImageUrl"
-			value={imageUrl}
-			on:input="{event => imageUrl = event.target.value}"
-			type="text"
-		/>
-		<TextInput 
-			id="email"
-			label="E-mail"
-			value={email}
-			on:input="{event => email = event.target.value}"
-			type="text"
-		/>
-		<TextInput 
-			id="description"
-			label="Description"
-			value={description}
-			on:input="{event => description = event.target.value}"
-			controlType="textarea"
-			type="textarea"
-		/>
-		<Button
-			type="submit"
-			caption="Save"
-		/>
-	</form>
-	<MeetupGrid meetups = { meetups } />
-</main>
+{#if showModal}
+<Modal on:cancel="{ () => showModal = false }" on:close="{ () => showModal = false }" let:didAgree={ closeable }>
+	<h1 slot="header">Hi</h1>
+	<p>This works really!</p>
+	<button type="button" slot="footer" on:click="{ () => showModal = false }" disabled={ !closeable }>Confirm</button>
+</Modal>
+{/if}
